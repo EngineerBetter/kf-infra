e40f12e8f005867a4671ea2a6f04e650d030692b
Tom Godkin <tom.godkin@engineerbetter.com>
2021-11-01 09:45:12 +0000

    Output GCP credentials, even though it is secret

--- terraform/project/outputs.tf
+++ terraform/project/outputs.tf
@@ -1,3 +1,4 @@
 output "gcp_credentials_json" {
-  value = base64decode(google_service_account_key.ci_bot_key.private_key)
+  sensitive = true
+  value     = base64decode(google_service_account_key.ci_bot_key.private_key)
 }
