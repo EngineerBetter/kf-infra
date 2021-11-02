c47b8b2ef0a2ed1ac9f78068948070a6f400bc61
Gustavo Carvalho <gustavo.carvalho@engineerbetter.com>
2021-11-02 08:45:14 -0300

    Using connection_name output instead of name
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@engineerbetter.com>

--- terraform/cloud-service-broker/outputs.tf
+++ terraform/cloud-service-broker/outputs.tf
@@ -2,9 +2,6 @@ output "service_account_email" {
   value = google_service_account.service_broker.email
 }
 
-output "csb_db_name" {
-  value = google_sql_database_instance.csb.name
-}
 output "csb_db_connecion_name" {
   value = google_sql_database_instance.csb.connection_name
 }
